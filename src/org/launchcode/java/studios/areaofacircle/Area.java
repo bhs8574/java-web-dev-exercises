666eef12a0b43bee0d371a76ddbd2de79d136e2f
bhs8574 <bhs8574@gmail.com>
2020-04-20 18:13:51 -0500

    Implemented the bonus mission do-while loop and a try/catch for exception handling!

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,9 +6,19 @@ public class Area {
 
     public static void main(String[] args) {
         double PI = 3.14159;
+        double radius;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
+        do {
+            try {
+                System.out.println("Enter a radius: ");
+                radius = input.nextDouble();
+            }
+            catch(Exception e) {
+                input.next();
+                System.out.println("Sorry, something went wrong!  Are you sure you entered a number?");
+                radius = -1.0;
+            }
+        } while (radius < 0 );
         System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
         input.close();
     }
