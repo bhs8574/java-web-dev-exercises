29f4247dfad0680957449cbb32c7cba87f5bddcd
bhs8574 <bhs8574@gmail.com>
2020-05-14 18:39:33 -0500

    Finished base exercise implementation for Interfaces/Polymorphism

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -10,10 +11,20 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-
+        Comparator comparator = new FlavorComparator();
+        flavors.sort(comparator);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
-
+        Comparator costComparator = new ConeComparator();
+        cones.sort(costComparator);
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("Flavors: ");
+        for(Flavor flavor: flavors ) {
+            System.out.println(flavor.getName());
+        }
+        System.out.println("\nCones: ");
+        for(Cone cone: cones){
+            System.out.println(cone.getName() + ": " + cone.getCost());
+        }
     }
 }
